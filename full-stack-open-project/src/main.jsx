d039a8751663bcfa76be51873b221dda4482a033
Noam Rosenberg <noamros9@gmail.com>
2026-04-23 12:49:26 +0300

    exc 1.12 done - anectode is being chosen randomly using a button

--- full-stack-open-project/src/main.jsx
+++ full-stack-open-project/src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
 
-import App from './part1/unicafe/UnicafeApp'
+import App from './part1/Anecdotes/AnecdotesApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)    
