662b778cbee09946b5fb27c73c33ca127ada8ffc
Noam Rosenberg <noamros9@gmail.com>
2026-04-27 10:40:04 +0300

    exc 2.1 done - CourseInfo app refactored

--- full-stack-open-project/src/components/CourseInfo/CourseInfoPart.jsx
+++ full-stack-open-project/src/components/CourseInfo/CourseInfoPart.jsx
@@ -1,5 +1,4 @@
 const CourseInfoPart = ({ part }) => {
-    console.log(part)
     return (<p>
         Part name: <b>{part.name}</b> - includes {part.exercises} excercises
     </p>
--- full-stack-open-project/src/part1/courseinfo/CourseInfoApp.jsx
+++ full-stack-open-project/src/part1/courseinfo/CourseInfoApp.jsx
@@ -1,32 +1,32 @@
-import CourseInfoHeader from '../../components/CourseInfo/CourseInfoHeader'
-import CourseInfoContent from '../../components/CourseInfo/CourseInfoContent'
-import CourseInfoTotal from '../../components/CourseInfo/CourseInfoTotal'
+import CourseInfo from '../../components/CourseInfo/CousreInfo'
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    }
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
-    }
+
     const courseInfo = {
-        name: course,
-        parts: [part1, part2, part3]
+        name: 'Half Stack application development',
+        id: 1,
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10,
+                id: 1
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7,
+                id: 2
+            },
+            {
+                name: 'State of a component',
+                exercises: 14,
+                id: 3
+            }
+        ]
     }
 
+
     return (
-        <div>
-            <CourseInfoHeader courseName={courseInfo.name} />
-            <CourseInfoContent courseContent={courseInfo.parts} />
-            <CourseInfoTotal courseContent={courseInfo.parts} />
-        </div>
+        <CourseInfo courseInfo={courseInfo} />
     )
 }
 
