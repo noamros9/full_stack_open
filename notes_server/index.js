72466769bf5689e62f7ae1d03b30d01ebafd3132
Noam Rosenberg <noamros9@gmail.com>
2026-05-16 20:51:34 +0300

    changed the notes_server backend to work fully with mongoDB

--- notes_server/index.js
+++ notes_server/index.js
@@ -1,29 +1,11 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Note = require('./mongo')
+const Note = require('./models/note')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
-
-let notes = [
-    {
-        id: "1",
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: "2",
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: "3",
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
 
 const generateId = () => {
     const maxId = notes.length > 0
@@ -43,40 +25,38 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    if (!note) {
-        response.status(404).end()
-    } else {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    }
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => {
+            console.log(error)
+            response.status(400).send({ error: 'malformatted id' })
+        })
 })
+
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
     if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
+        return response.status(400).json({ error: 'content missing' })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 const PORT = process.env.PORT || 3001
--- notes_server/models/note.js
+++ notes_server/models/note.js
@@ -10,11 +10,24 @@ const url = `mongodb+srv://${username}:${password}@${clusterAddress}/${dbName}?r
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url, { family: 4 })
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch(error => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
 
 const noteSchema = new mongoose.Schema({
     content: String,
     important: Boolean
 })
-const Note = mongoose.model('Note', noteSchema)
 
-module.exports = Note
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
